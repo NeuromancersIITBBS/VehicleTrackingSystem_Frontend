f7ba5f32b4cca01e6919b23edcd197f8f04b287c
RajShah-1 <srk13@iitbbs.ac.in>
2020-01-24 20:00:09 +0530

    Debugged routes on backend and changed endpoints in user models

--- TestBackend/index.js
+++ TestBackend/index.js
@@ -1,10 +1,18 @@
 const socket = require('socket.io');
+const bodyParser = require('body-parser');
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+app.use(express.static('../VTSFrontend'));
 
 let server = app.listen(4000, () => {console.log('Now listening to port 4000!')});
 
 let io = socket(server);
+// To be moved to backend
+let users = [];
 
 io.on('connection', (socket) => {
     console.log('made socket connection on socket: ' + socket.id);
@@ -18,4 +26,26 @@ io.on('connection', (socket) => {
     // });
 });
 
+// BOOK Request: /book (POST)
+app.post('/book', (req, res) => {
+    let user = req.body;
+    console.log('Book Request');
+    console.log(user);
+    users.push(user);
+    uniqueID = 11;
+    // Create a session using cookie-parser/sessions
+    res.send(uniqueID.toString());
+});
 
+// UNBOOK Request: /unbook/:id GET
+app.get('/unbook/:id', (req, res) => {
+    // const userId = req.params.id;
+    // Find the userId in the array and delete that user
+    console.log('Unbook Request from ' + id);
+    res.end();
+});
+
+app.post('/gotin/:id', (req, res) => {
+    console.log('Gotin Request from ' + id);
+    res.end();
+});
--- VTSFrontend/models/userModel.js
+++ VTSFrontend/models/userModel.js
@@ -1,6 +1,6 @@
 //BOOK function to update the user's location and his destination (ajax POST)
 function book (pickupObject) {
-	let endpoint = `http:\\vts_backend`;
+	let endpoint = `http://localhost:4000/book`;
 	let jsObj = {
 		location: pickupObject,
 		destination: $("#destination").val(),
@@ -14,6 +14,7 @@ function book (pickupObject) {
 		contentType: 'application/json',
 	}).done(function (response) {
 		console.log('Success');
+		console.log(response);
 		return response;
 	}).fail(function () {
 		alert('fail!');
@@ -22,8 +23,8 @@ function book (pickupObject) {
 }
 /////// ajax call for UNBOOKING
 function unBook(uniqueId) {
-	let endpoint = `http:\\vts_backend`;
-	const userId = $.ajax({
+	let endpoint = `http://localhost:4000/unbook/${uniqueId}`;
+	$.ajax({
 		url: endpoint,
 		method: 'GET',
 		dataType: 'json',
@@ -38,11 +39,10 @@ function unBook(uniqueId) {
 }
 ///////////// ajax call for GOTIN
 function gotIn(uniqueId) {
-	let endpoint = `http:\\vts_backend`;
+	let endpoint = `http://localhost:4000/gotin/${uniqueId}`;
 	let jsObj = {
-		Id: uniqueId,
+		id: uniqueId,
 	};
-
 	let jsonObj = JSON.stringify(jsObj);
 	$.ajax({
 		url: endpoint,
