334f6cdd86d1a4176c4dab2db8ac508f48f7627a
GaneshRamKoushik <grk14@iitbbs.ac.in>
2020-02-13 18:31:35 +0530

    Initial CSS done

--- VTSFrontend/controllers/cookiesController.js
+++ VTSFrontend/controllers/cookiesController.js
@@ -1,37 +1,36 @@
 // expiresMin, path and domain are optional arguments
 function createCookie(name, value, expires, path, domain) {
-	var cookie = name + "=" + escape(value) + ";";
+    var cookie = name + "=" + escape(value) + ";";
 
-	if (expires) {
-		// If it's a date
-		if (expires instanceof Date) {
-			// If it isn't a valid date
-			if (isNaN(expires.getTime()))
-				expires = new Date();
-		}
-		else
-			expires = new Date(new Date().getTime() + parseInt(expires) * 1000 * 60 );
+    if (expires) {
+        // If it's a date
+        if (expires instanceof Date) {
+            // If it isn't a valid date
+            if (isNaN(expires.getTime()))
+                expires = new Date();
+        } else
+            expires = new Date(new Date().getTime() + parseInt(expires) * 1000 * 60);
 
-		cookie += "expires=" + expires.toGMTString() + ";";
-	}
+        cookie += "expires=" + expires.toGMTString() + ";";
+    }
 
-	if (path)
-		cookie += "path=" + path + ";";
-	if (domain)
-		cookie += "domain=" + domain + ";";
-	console.log(cookie);
-	document.cookie = cookie;
+    if (path)
+        cookie += "path=" + path + ";";
+    if (domain)
+        cookie += "domain=" + domain + ";";
+    console.log(cookie);
+    document.cookie = cookie;
 }
 
 function getCookie(name) {
-	let regexp = new RegExp("(?:^" + name + "|;\s*" + name + ")=(.*?)(?:;|$)", "g");
-	let result = regexp.exec(document.cookie);
-	return (result === null) ? null : result[1];
+    let regexp = new RegExp("(?:^" + name + "|;\s*" + name + ")=(.*?)(?:;|$)", "g");
+    let result = regexp.exec(document.cookie);
+    return (result === null) ? null : result[1];
 }
 
 // path and domain are optional arguments
 function deleteCookie(name, path, domain) {
-	// If the cookie exists
-	if (getCookie(name))
-		createCookie(name, "", -1, path, domain);
+    // If the cookie exists
+    if (getCookie(name))
+        createCookie(name, "", -1, path, domain);
 }
