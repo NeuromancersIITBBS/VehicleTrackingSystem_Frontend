2a1b2ccb717cf9378aa87abcf0db6a4c1bdc82b6
Raj Shah <srk13@iitbbs.ac.in>
2020-04-01 15:19:42 +0530

    Moved BookBtn Listener out of the if-else statements

--- VTSFrontend/views/userSocket.js
+++ VTSFrontend/views/userSocket.js
@@ -4,50 +4,51 @@ let socket = io.connect('http://localhost:3000');
 let uniqueId;
 
 $(document).ready(async function () {
-	if(JSON.parse(localStorage.getItem('userData'))==null){
-		console.log("New Session !!")
-		$('#confirmBook').click(async function () {
-			//alert if the pickup and drop locations are same-
-			if($('#userLocation').val()===$('#destination').val()){
-				alert("The Pickup location and Destination are same !");
-			} else{
-			let pickupObj = {};
+	$('#confirmBook').click(async function () {
+		if(JSON.parse(localStorage.getItem('userData'))==null){
+			console.log("New Session !!")
+		}
+		else{
+			console.log("Welcome Back !!")
 			$('#bookIn').hide();
 			$('#bookOut').show();
-			
-			// Get GPS location of User
-			
-			if($('#userLocation').val()==='YourLocation'){
-				if(navigator.geolocation) {
-					navigator.geolocation.getCurrentPosition(async (position) => {
-						pickupObj = {
-							pickupPoint: 'custom',
-							location: {
-								lat: position.coords.latitude,
-								lng: position.coords.longitude
-							}
-						};
-						uniqueId = await bookController(pickupObj);
-					});
-				} else {
-					alert('Geolocation is not supported by this browser.');
-			  	}
-			}
-			else{
-				pickupObj = {
-					pickupPoint: $('#userLocation').val(),
-					
-					// needs to be updated
-				};
-				uniqueId = await bookController(pickupObj);
-			}	}
-
-		});}
-	else{
-		console.log("Welcome Back !!")
+		}
+		//alert if the pickup and drop locations are same-
+		if($('#userLocation').val()===$('#destination').val()){
+			alert("The Pickup location and Destination are same !");
+		} else{
+		let pickupObj = {};
 		$('#bookIn').hide();
 		$('#bookOut').show();
-	}
+		
+		// Get GPS location of User
+		
+		if($('#userLocation').val()==='YourLocation'){
+			if(navigator.geolocation) {
+				navigator.geolocation.getCurrentPosition(async (position) => {
+					pickupObj = {
+						pickupPoint: 'custom',
+						location: {
+							lat: position.coords.latitude,
+							lng: position.coords.longitude
+						}
+					};
+					uniqueId = await bookController(pickupObj);
+				});
+			} else {
+				alert('Geolocation is not supported by this browser.');
+			  }
+		}
+		else{
+			pickupObj = {
+				pickupPoint: $('#userLocation').val(),
+				
+				// needs to be updated
+			};
+			uniqueId = await bookController(pickupObj);
+		}	}
+
+	});
 
 	$('#unBook').click(function () {
 		$('#bookOut').hide();
